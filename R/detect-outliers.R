c3fd07133092f732267f4d1ad116dce58e56bc1a
Thibaut Jombart <thibautjombart@gmail.com>
2020-05-29 10:48:04 +0100

    adding outlier detection

---
+++ R/detect-outliers.R
@@ -0,0 +1,15 @@
+#' @export
+detect_outliers <- function(model, data, alpha = 0.05, ...) {
+  data <- dplyr::select(data, ..., everything())
+  preds <- model$train(data)$predict(data, alpha = alpha)
+  out <- dplyr::mutate(
+    preds,
+    outlier = observed < lower | observed > upper,
+    classification = case_when(
+      observed < lower ~ "decrease",
+      observed > upper ~ "increase",
+      TRUE ~ "normal"),
+    classification = factor(classification,
+                            levels = c("increase", "normal", "decrease")))
+  out
+}
