20e90a68fd698f270f49cb41ebf1b8352bd7ff22
Thibaut Jombart <thibautjombart@gmail.com>
2020-05-30 18:42:44 +0100

    adding and using accessors for epichange objects

--- R/plot.R
+++ R/plot.R
@@ -1,6 +1,7 @@
 #' @export
   
 plot.epichange <- function(x,
+                           x_axis, 
                            point_size = 2,
                            col_normal = "#8B8B8C",
                            col_increase = "#CB3355",
@@ -8,7 +9,22 @@ plot.epichange <- function(x,
                            guide = TRUE,
                            ...) {
 
-  stop_train <- max(x$results$day) - x$k
+  ## ensure that x_axis is the name of a variable
+  results <- get_results(x)
+  if (is.numeric(x_axis)) {
+    x_axis <- names(results)[x_axis]
+  }
+
+  n <- nrow(results)
+  n_train <- n - get_k(x)
+  if (n_train < n) {
+    train_limit <- mean(results[n_train:(n_train + 1), x_axis])
+  } else {
+    train_limit <- NULL
+  }
+  
+
+  
   col_model <- "#BBB67E"
   
   scale_classification <- ggplot2::scale_color_manual(
@@ -19,14 +35,13 @@ plot.epichange <- function(x,
 
   custom_guide <- if (guide) ggplot2::guide_legend(override.aes = list(size = c(4, 4, 3))) else FALSE
   
-  ggplot2::ggplot(x$results, aes(x = day, y = count)) +
+  ggplot2::ggplot(results, ggplot2::aes_string(x = x_axis, y = "count")) +
     ggplot2::theme_bw() +
-      ##ggplot2::large_txt +
-      ggplot2::geom_vline(xintercept = stop_train, linetype = 2) +
+      ggplot2::geom_vline(xintercept = train_limit, linetype = 2) +
       ggplot2::geom_ribbon(ggplot2::aes(ymin = lower, ymax = upper),
                            alpha = 0.4, fill = col_model) +
       ggplot2::geom_point(ggplot2::aes(color = classification),
-                          size = point_size + x$results$outlier) +
+                          size = point_size + results$outlier) +
       ggplot2::geom_line(alpha = 0.3) +
       scale_classification +
       ggplot2::theme(legend.position = "bottom") +
