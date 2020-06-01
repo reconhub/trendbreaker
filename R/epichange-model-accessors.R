308c478fea445cf4a6d919e8eb2732ea4265cc71
Thibaut Jombart <thibautjombart@gmail.com>
2020-06-01 13:31:27 +0100

    splitting model accessors in 2 files

--- R/epichange-model-accessors.R
+++ R/epichange-model-accessors.R
@@ -1,7 +1,7 @@
 
 #' Accessors for epichange_model objects
 #'
-#' These functions can be used to access information stored in `epichange`
+#' These functions can be used to access information stored in `epichange_model`
 #' objects. See details.
 #'
 #' @details The following accessors are available:
@@ -12,14 +12,19 @@
 #' 
 #' * `get_family`: get the model family, indicating the type of distribution
 #' assumed for the response variable
+#'
+#' * `train`: train a model using data to obtain a
+#' [`epichange_model_fit`](epichange_model_fit) object
 #' 
 #' @author Thibaut Jombart, Dirk Schumacher
-#' 
-#' @aliases epichange_model-accessors epichange_model-class
 #' 
 #' @param model the output of functions `lm_model`, `glm_model`, or
 #'   `glm_nb_model`
 #' 
+#' @param data a `data.frame` to be used to train the model#'
+#' 
+#' @aliases epichange_model-accessors epichange_model-class
+
 #' @export
 #' @rdname epichange_model-accessors
 #' @aliases get_formula get_formula.epichange 
@@ -41,5 +46,17 @@ get_response.epichange_model <- function(model) {
 #' @rdname epichange_model-accessors
 #' @aliases get_family get_family.epichange_model
 get_family.epichange_model <- function(model) {
-  as.list(environment(model$train))$family
+  if (inherits(model, "epichange_lm")) {
+    "gaussian"
+  } else {
+    as.list(environment(model$train))$family
+  }
+}
+
+
+#' @export
+#' @rdname epichange_model-accessors
+#' @aliases train
+train.epichange_model <- function(model, data) {
+  model$train(data)
 }
---
+++ R/epichange-model-fit-accessors.R
@@ -0,0 +1,45 @@
+
+#' Accessors for epichange_model_fit objects
+#'
+#' These functions can be used to access information stored in
+#' `epichange_model_fit` objects. See details.
+#'
+#' @details The following accessors are available:
+#'
+#' * `get_formula`: get the formula used to model temporal trends
+#' 
+#' * `get_response`: get the name of the response variable
+#' 
+#' * `get_family`: get the model family, indicating the type of distribution
+#' assumed for the response variable
+#' 
+#' @author Thibaut Jombart, Dirk Schumacher
+#' 
+#' @param model the output of functions `lm_model`, `glm_model`, or
+#'   `glm_nb_model`
+#' 
+#' @param x the output of functions `lm_model`, `glm_model`, or
+#'   `glm_nb_model`
+#'
+#' 
+#' @aliases epichange_model_fit-accessors epichange_model_fit-class
+
+#' @export
+#' @rdname epichange_model_fit-accessors
+#' @aliases get_model.epichange
+get_model.epichange_model_fit <- function(x) {
+  x$model
+}
+
+
+#' @export
+#' @rdname epichange_model_fit-accessors
+#' @aliases predict.epichange_model_fit
+#' @param object an `epichange_model_fit` object
+#' @param newdata a `data.frame` containing data for which predictions are to be
+#'   derived
+#' @param alpha the alpha threshold to be used for prediction intervals,
+#'   defaulting to 0.05, i.e. 95% prediction intervals are derived
+predict.epichange_model_fit <- function(object, newdata, alpha = 0.05, ...) {
+  get_model(object)$predict(newdata = newdata, alpha = alpha)
+}
