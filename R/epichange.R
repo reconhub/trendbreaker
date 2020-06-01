fe24ecda8e8b5f23acfb0c5dbe8e2963a408ede8
Thibaut Jombart <thibautjombart@gmail.com>
2020-06-01 16:41:25 +0100

    adding example to asmodee

--- R/epichange.R
+++ R/epichange.R
@@ -63,6 +63,55 @@
 #' @return An `epichange` object (S3 class inheriting `list`), containing items
 #'   which can be accessed by various accessors - see `?epichange-accessors`
 #' 
+#' @examples
+#'
+#' if (require(cowplot)) {
+#'   # load data
+#'   data(nhs_pathways_covid19)
+#'
+#'   # define candidate models
+#'   models <- list(
+#'     regression = lm_model(count ~ day),
+#'     poisson_constant = glm_model(count ~ 1, family = "poisson"),
+#'     negbin_time = glm_nb_model(count ~ day),
+#'     negbin_time_weekday = glm_nb_model(count ~ day + weekday)
+#'   )
+#'
+#'   # analyses on all data
+#'   counts_overall <- pathways_recent %>%
+#'     group_by(date, day, weekday) %>%
+#'     summarise(count = sum(count))
+#'
+#'   # results with automated detection of 'k'
+#'   res_overall <- asmodee(counts_overall, models, method = evaluate_aic)
+#'   res_overall
+#'   plot(res_overall, "date")
+#'
+#'   # results with fixed value of 'k' (7 days)
+#'   res_overall_k7 <- asmodee(counts_overall, models, fixed_k = 7)
+#'   plot(res_overall_k7, "date")
+#'
+#'   # analyses by NHS regions
+#'   counts_nhs_region <- pathways_recent %>%
+#'     group_by(nhs_region, date, day, weekday) %>%
+#'     summarise(count = sum(count)) %>%
+#'     complete(date, fill = list(count = 0)) %>% 
+#'     split(.$nhs_region)
+#'
+#'   res_nhs_region <- lapply(counts_nhs_region,
+#'                            asmodee,
+#'                            models,
+#'                            method = evaluate_aic,
+#'                            alpha = 0.05)
+#'
+#'   plots_nhs_region <- lapply(seq_along(res_nhs_region),
+#'                              function(i)
+#'                                plot(res_nhs_region[[i]], "date", point_size = 1, guide = FALSE) +
+#'                                  labs(subtitle = names(res_nhs_region)[i], x = NULL))
+#'   cowplot::plot_grid(plotlist = plots_nhs_region)
+#'
+#' }
+
 asmodee <- function(data,
                     models,
                     alpha = 0.05,
