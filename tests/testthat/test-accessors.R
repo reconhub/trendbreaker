7012c16fe420c6bcef026caea94216bcf110250f
Tim Taylor <tim.taylor@hiddenelephants.co.uk>
2020-08-25 12:49:35 +0100

    more fixes due to different branches

--- tests/testthat/test-accessors.R
+++ tests/testthat/test-accessors.R
@@ -14,15 +14,15 @@ test_that("trendbreaker accessors", {
   expect_output(print(get_model(res)), "y ~ x")
   expect_equal(get_k(res), 1)
 
-  nms <- c(colnames(dat), "pred", "lower-ci", "upper-ci", "lower-pi", "upper-pi", "observed", "outlier", "classification")
+  nms <- c(colnames(dat), "pred", "lower-pi", "upper-pi", "observed", "outlier", "classification")
   expect_equal(names(get_results(res)), nms)
 
   outliers <- get_outliers(res)
   expect_equal(nrow(outliers), 0)
-  expect_equal(ncol(outliers), 10)
+  expect_equal(ncol(outliers), 8)
 
   pred <- predict(res, dat)
-  nms <- c(colnames(dat), "pred", "lower-ci", "upper-ci", "lower-pi", "upper-pi", "observed")
+  nms <- c(colnames(dat), "pred", "lower-pi", "upper-pi", "observed")
   expect_equal(pred$pred, y)
   expect_equal(names(pred), nms)
 
