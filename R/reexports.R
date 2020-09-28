ccc187aa4faba34c63c842fc53c2d4e5c531b997
Tim Taylor <tim.taylor@hiddenelephants.co.uk>
2020-09-28 13:44:38 +0100

    rexport evaluate_aic and evaluate_resampling from trendeval

--- R/reexports.R
+++ R/reexports.R
@@ -21,3 +21,15 @@ trending::glm_nb_model
 #' @export
 #' @name brms_model
 trending::brms_model
+
+#' @title Evaluate aic
+#' @importFrom trendeval evaluate_aic
+#' @export
+#' @name evaluate_aic
+trendeval::evaluate_aic
+
+#' @title Evaluate resampling
+#' @importFrom trendeval evaluate_resampling
+#' @export
+#' @name evaluate_resampling
+trendeval::evaluate_resampling
